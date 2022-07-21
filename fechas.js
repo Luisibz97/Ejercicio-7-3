93f02101788477755da4dd1d8714a73c0b5e9f70
Luisibz97 <luis_rab_20@hotmail.com>
2022-07-21 19:40:02 -0300

    first commit

---
+++ fechas.js
@@ -0,0 +1,14 @@
+const currentDate = new Date()
+
+const myBirthday = new Date(1997, 8, 5)
+
+const dateComparison = currentDate > myBirthday
+
+const myBirthdayDay = myBirthday.getDay()
+
+const myBirthdayMonth = myBirthday.getMonth() + 1
+
+const myBirthdayYear = myBirthday.getFullYear()
+
+
+console.log(myBirthdayYear);
